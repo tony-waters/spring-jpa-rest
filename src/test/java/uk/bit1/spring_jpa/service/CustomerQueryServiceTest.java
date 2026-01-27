b27c23e5ef87d861aed9710578c3bf73961c6a4b
tony-waters <2tonywaters@gmail.com>
2026-01-27 01:10:31 +0000

    simplify service architecture

--- src/test/java/uk/bit1/spring_jpa/service/CustomerQueryServiceTest.java
+++ src/test/java/uk/bit1/spring_jpa/service/CustomerQueryServiceTest.java
@@ -2,16 +2,14 @@ package uk.bit1.spring_jpa.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.PageRequest;
-import uk.bit1.spring_jpa.service.impl.CustomerQueryServiceImpl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@Import(CustomerQueryServiceImpl.class)
+@Import(CustomerQueryService.class)
 class CustomerQueryServiceTest {
 
     @Autowired
@@ -21,6 +19,6 @@ class CustomerQueryServiceTest {
     void listCustomers_returnsPagedProjection() {
         var page = customerQueryService.listCustomers(PageRequest.of(0, 10));
         assertThat(page).isNotNull();
-        // you can assert size/content depending on your seeded test data
+        // can assert size/content depending on seeded test data
     }
 }
