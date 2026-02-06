b7aed1b23b8fc257e11ff92e56f2c0fb915954df
tony-waters <2tonywaters@gmail.com>
2026-02-06 14:43:15 +0000

    refactor DTOs

--- src/main/java/uk/bit1/spring_jpa/service/dto/CustomerDetailCreateDto.java
+++ src/main/java/uk/bit1/spring_jpa/service/dto/CustomerDetailCreateDto.java
@@ -4,61 +4,19 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public class CustomerDetailCreateDto {
+public record CustomerDetailCreateDto(
+        @NotBlank(message = "First Name is required")
+        @Size(min = 2, max = 50, message = "First Name must be 2-50 characters")
 
-    @NotBlank(message = "First Name is required")
-    @Size(min = 2, max = 50, message = "First Name must be 2-50 characters")
-    private final String firstName;
+        String firstName,
+        @NotBlank(message = "Last Name is required")
+        @Size(min = 2, max = 50, message = "Last Name must be 2-50 characters")
 
-    @NotBlank(message = "Last Name is required")
-    @Size(min = 2, max = 50, message = "Last Name must be 2-50 characters")
-    private final String lastName;
+        String lastName,
+        @Email(message = "Email must be valid")
+        @NotBlank(message = "Email is required")
 
-    @Email(message = "Email must be valid")
-    @NotBlank(message = "Email is required")
-    private final String email;
-
-    @NotBlank(message = "Phone Number is required")
-    @Size(min = 11, max = 20, message = "Phone Number must be 11-20 characters")
-    private final String phoneNumber;
-
-    public CustomerDetailCreateDto(String firstName, String lastName, String email, String phoneNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-
-    public String getEmail() {
-        return email;
-    }
-
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-//    public void setPhoneNumber(String phoneNumber) {
-//        this.phoneNumber = phoneNumber;
-//    }
-
-}
+        String email,
+        @NotBlank(message = "Phone Number is required")
+        @Size(min = 11, max = 20, message = "Phone Number must be 11-20 characters")
+        String phoneNumber) {}
--- src/main/java/uk/bit1/spring_jpa/service/dto/CustomerDetailDto.java
+++ src/main/java/uk/bit1/spring_jpa/service/dto/CustomerDetailDto.java
@@ -4,82 +4,22 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public class CustomerDetailDto {
+public record CustomerDetailDto(
+        Long id,
+        Long version,
 
-    private final Long id;
+        @NotBlank(message = "First Name is required")
+        @Size(min = 2, max = 50, message = "First Name must be 2-50 characters")
+        String firstName,
 
-    private final Long version;
+        @NotBlank(message = "Last Name is required")
+        @Size(min = 2, max = 50, message = "Last Name must be 2-50 characters")
+        String lastName,
 
-    @NotBlank(message = "First Name is required")
-    @Size(min = 2, max = 50, message = "First Name must be 2-50 characters")
-    private final String firstName;
+        @Email(message = "Email must be valid")
+        @NotBlank(message = "Email is required")
+        String email,
 
-    @NotBlank(message = "Last Name is required")
-    @Size(min = 2, max = 50, message = "Last Name must be 2-50 characters")
-    private final String lastName;
-
-    @Email(message = "Email must be valid")
-    @NotBlank(message = "Email is required")
-    private final String email;
-
-    @NotBlank(message = "Phone Number is required")
-    @Size(min = 11, max = 20, message = "Phone Number must be 11-20 characters")
-    private final String phoneNumber;
-
-    public CustomerDetailDto(Long id, Long version, String firstName, String lastName, String email, String phoneNumber) {
-        this.id = id;
-        this.version = version;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-
-    public String getEmail() {
-        return email;
-    }
-
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-//    public void setPhoneNumber(String phoneNumber) {
-//        this.phoneNumber = phoneNumber;
-//    }
-
-    public Long getVersion() {
-        return version;
-    }
-
-//    public void setVersion(Long version) {
-//        this.version = version;
-//    }
-}
+        @NotBlank(message = "Phone Number is required")
+        @Size(min = 11, max = 20, message = "Phone Number must be 11-20 characters")
+        String phoneNumber) {}
