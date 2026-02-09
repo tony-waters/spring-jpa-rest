69140ff53be3168cb91f13436ab4bd81a956b1ba
tony-waters <2tonywaters@gmail.com>
2026-02-09 18:01:14 +0000

    tidy up

--- src/main/java/uk/bit1/spring_jpa/service/dto/OrderDto.java
+++
@@ -1,6 +0,0 @@
-package uk.bit1.spring_jpa.service.dto;
-
-import java.util.List;
-
-public record OrderDto(Long id, String description, Boolean fulfilled, CustomerDetailDto customer, List<ProductDto> products) {
-}
--- src/main/java/uk/bit1/spring_jpa/service/dto/ProductDto.java
+++
@@ -1,4 +0,0 @@
-package uk.bit1.spring_jpa.service.dto;
-
-public record ProductDto(Long id, String name, String description) {
-}
